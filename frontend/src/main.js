5eec4606d753c7a318b11ec06dc27a614892dcf8
hadidharmawan <hadidharmawan32@gmail.com>
2021-03-16 08:38:40 +0700

    edit main js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,10 +10,16 @@ Vue.component('apexchart', VueApexCharts)
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
-import * as Wails from '@wailsapp/runtime';
+import Bridge from './wailsbridge';
+// import * as Wails from '@wailsapp/runtime';
 
-Wails.Init(() => {
+// Wails.Init(() => {
+// 	new Vue({
+// 		render: h => h(App)
+// 	}).$mount('#app');
+// });
+Bridge.Start(() => {
 	new Vue({
 		render: h => h(App)
-	}).$mount('#app');
+	}).$mount("#app");
 });
